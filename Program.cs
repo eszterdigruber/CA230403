13a6759defc0a14ce0ad1230924f05f75947fc86
digruber.eszter <digruber.eszter@28-01>
2023-04-03 13:23:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace CA230403
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            using StreamWriter sw = new(path: 
+                "valami.txt", 
+                append: false, 
+                encoding: Encoding.UTF8);
+
+            DateTime birthDate = new(2005, 12, 12);
+            string name = "Digruber Eszter";
+
+            sw.WriteLine($"Hello, my name is {name}, " +
+                $"I was born {DateTime.Today.Year - birthDate.Year} years ago.");
+
+            Console.WriteLine("done!");
+        }
+    }
+}
